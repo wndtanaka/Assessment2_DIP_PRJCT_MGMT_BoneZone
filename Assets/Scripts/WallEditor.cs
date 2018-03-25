ab54079d49c312a6e47a75f6a4b3c6931d9b6213
Unknown <wndtanaka@gmail.com>
2018-03-25 15:05:59 +1100

    MVP v 0.0.5

---
+++ Assets/Scripts/WallEditor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallEditor : MonoBehaviour
+{
+    public float rayRange = 5f;
+
+    Vector3 rayOffset = new Vector3(0, 3, 0);
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position,Camera.main.transform.forward, out hit,rayRange))
+        {
+            CheckWall(hit);
+        }
+    }
+    void CheckWall(RaycastHit hit)
+    {
+        if (hit.transform.gameObject.layer == LayerMask.NameToLayer("MoveableWall"))
+        {
+            Debug.Log("Wall Detected");
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position + rayOffset, transform.position + rayOffset + Vector3.forward * rayRange);
+    }
+}
