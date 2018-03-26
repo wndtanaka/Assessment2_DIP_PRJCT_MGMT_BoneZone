256cc8c3f46812353ef9b081a45ded1468064e86
Unknown <wndtanaka@gmail.com>
2018-03-26 11:53:51 +1100

    MVP v 0.0.6

--- Assets/Scripts/WallEditor.cs
+++ Assets/Scripts/WallEditor.cs
@@ -6,33 +6,43 @@ public class WallEditor : MonoBehaviour
 {
     public float rayRange = 5f;
 
-    Vector3 rayOffset = new Vector3(0, 3, 0);
+    MovableWall movableWall;
+    Vector3 offset;
 
     // Use this for initialization
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        offset = transform.position + new Vector3(0, 3, 0);
+        // press Q and E to move the wall to the left and right respectively
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            CheckWall();
+        }
+    }
+    void CheckWall()
+    {
+        // checking the wall in rayRange if there is any wall with "MoveableWall" layer attached
         RaycastHit hit;
-        if (Physics.Raycast(Camera.main.transform.position,Camera.main.transform.forward, out hit,rayRange))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, rayRange))
         {
-            CheckWall(hit);
+            if (hit.transform.gameObject.layer == LayerMask.NameToLayer("ShiftableWall"))
+            {
+                movableWall = hit.transform.GetComponent<MovableWall>();
+                Debug.Log("Shifting Wall");
+                movableWall.ShiftWall();
+            }
+            if (hit.transform.gameObject.layer == LayerMask.NameToLayer("HackableWall"))
+            {
+                movableWall = hit.transform.GetComponent<MovableWall>();
+                Debug.Log("Hacking Wall");
+                movableWall.HackWall();
+            }
         }
-    }
-    void CheckWall(RaycastHit hit)
-    {
-        if (hit.transform.gameObject.layer == LayerMask.NameToLayer("MoveableWall"))
-        {
-            Debug.Log("Wall Detected");
-        }
-    }
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position + rayOffset, transform.position + rayOffset + Vector3.forward * rayRange);
     }
 }
