45345151857c5724ef5249e4923cf6f09b4341c4
EricW <lee21acc973@163.com>
2018-05-09 10:27:10 +1000

    added player health bar, modified menu UI and environment

---
+++ Assets/Dissolver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolver : MonoBehaviour
+{
+    public Material dissolveMaterial;
+
+    public float speed, max;
+
+    private float currentY, startTime;
+
+    private void Update()
+    {
+        // if this is during the animation
+        if (currentY < max)
+        {
+            dissolveMaterial.SetFloat("_DissolveY", currentY);
+            currentY += Time.deltaTime * speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            Dissolve();
+        }
+    }
+
+    void Dissolve()
+    {
+        startTime = Time.time;
+        currentY = 0;
+    }
+}
---
+++ Assets/Menus/Scripts/GameMenu.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public AudioSource hoverSFX, clickSFX;
+
+    private Animator anim;
+    private bool isPaused = false;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        HideCursor();
+    }
+
+    void HideCursor()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void ShowCursor()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && !isPaused)
+        {
+            OpenPause();
+            ShowCursor();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && isPaused)
+            Resume();
+
+        if (Input.GetKeyDown(KeyCode.Escape) && isPaused)
+        {
+            Debug.Log("Quit");
+            Quit();
+        }
+    }
+
+    #region Pause Menu
+
+    IEnumerator PauseCountdown()
+    {
+        yield return new WaitForSeconds(0.15f);
+        Time.timeScale = 0;
+    }
+
+    public void OpenPause()
+    {
+        playClickSound();
+        anim.Play("PauseIn");
+        ShowCursor();
+        StartCoroutine("PauseCountdown");
+        isPaused = true;
+        Debug.Log("Is Paused");
+    }
+    
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        playClickSound();
+        anim.Play("PauseOut");
+        HideCursor();
+        isPaused = false;
+        Debug.Log("Is not Paused");
+    }
+
+    public void Restart()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Time.timeScale = 1;
+        playClickSound();
+        SceneManager.LoadScene("MainMenu");
+    }
+    #endregion
+
+    #region Sounds
+    public void playHoverClip()
+    {
+        hoverSFX.Play();
+    }
+
+    public void playClickSound()
+    {
+        clickSFX.Play();
+    }
+
+    #endregion
+}
